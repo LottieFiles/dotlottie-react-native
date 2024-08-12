0added6f8956e0f06f5c2db94f948886f21c1431
Shamsudeen Yusuf <shamoyusuf@gmail.com>
2024-08-12 12:51:22 +0100

    chore: basic dotlottie configuration for android

--- example/android/app/src/main/java/dotlottiereactnative/example/MainActivity.kt
+++ example/android/app/src/main/java/dotlottiereactnative/example/MainActivity.kt
@@ -11,12 +11,12 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "dotlottiereactnative.example"
+  override fun getMainComponentName(): String = "DotlottieReactNativeExample"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+          DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const LINKING_ERROR =
   '- You are not using Expo Go\n';
 
 type DotlottieReactNativeProps = {
-  color: string;
+  source: string;
   style: ViewStyle;
 };
 
