c8bcf20944626c6ea50abc846fae8a5fae60d654
Abdelrahman Ashraf <a.theashraf@gmail.com>
2026-03-31 10:21:59 +0700

    chore: update dotlottie-android to v0.13.4

--- example/examples/MultipleAnimationsTest.tsx
+++ example/examples/MultipleAnimationsTest.tsx
@@ -35,7 +35,7 @@ export function MultipleAnimationsTest() {
               style={styles.animation}
               autoplay
               loop
-              // renderer='gl'
+              renderer='gl'
             />
           </View>
         ))}
